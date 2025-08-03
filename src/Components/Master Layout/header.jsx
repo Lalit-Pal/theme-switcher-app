8605b6e14abdac678c7202836f1a3b3a0501bc43
Lalit-Pal <lalitrai216@gmail.com>
2025-08-03 12:11:22 +0530

    updated header

--- src/Components/Master Layout/header.jsx
+++ src/Components/Master Layout/header.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { switchTheme } from "../../Redux/themeSlice";
 import styles from "./header.module.css";
 import { useNavigate } from "react-router";
+
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -10,45 +11,23 @@ const Header = () => {
   );
 
   return (
-    <div className={`${styles[selectedTheme]} ${styles.headerContainer} `}>
+    <div className={`${styles[selectedTheme]} ${styles.headerContainer}`}>
       <h1>Theme Switcher</h1>
       <div className={styles.navigation}>
-        <a
-          href="#"
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Home
-        </a>
-        <a
-          href="#"
-          onClick={() => {
-            navigate("/about");
-          }}
-        >
-          About
-        </a>
-        <a
-          href="#"
-          onClick={() => {
-            navigate("/contact");
-          }}
-        >
-          Contact
-        </a>
+        <button onClick={() => navigate("/")}>Home</button>
+        <button onClick={() => navigate("/about")}>About</button>
+        <button onClick={() => navigate("/contact")}>Contact</button>
         <select
-          onChange={(e) => {
-            dispatch(switchTheme(e.target.value));
-          }}
+          onChange={(e) => dispatch(switchTheme(e.target.value))}
           value={selectedTheme}
         >
-          <option value={"light"}>Light</option>
-          <option value={"dark"}>Dark</option>
-          <option value={"colorful"}>Colorful</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          <option value="colorful">Colorful</option>
         </select>
       </div>
     </div>
   );
 };
+
 export default Header;
